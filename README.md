6e22e174f788aac6b499bf55af73874a8f012c10
Mkozi <mkozi.junk@gmail.com>
2021-10-03 21:08:08 +0000

    Valid arg list

--- README.md
+++ README.md
@@ -1,15 +1,56 @@
 # Donald Feury: Golang Cobra Tutorial
+
 From youtube: https://www.youtube.com/watch?v=7U12a-TTtfo
 Cobra Project: [github](https://github.com/spf13/cobra)
 
 ## Examples
-```
-$ go run ./cobra-example/  echo times -h
-$ go run ./cobra-example/ echo times abcd efg --persistFlag=false -t 3
+* Root: For boolean flags,set default to false, then pass empty flag to set to true
 
-# required flag
-go run ./cobra-example/  echo times abcd efg
+  ```bash
+  go run ./cobra-example/main.go
+  go run ./cobra-example/main.go --help
 
-# to see returned custom error
-$ go run ./cobra-example/  echo times abcd efg --persistFlag=true -t 4
-```
+  go run ./cobra-example/main.go -p -l
+  go run ./cobra-example/main.go -pl
+  go run ./cobra-example/main.go --localFlag --persistFlag
+
+  ```
+
+* Echo
+
+  ```bash
+  go run ./cobra-example/  echo -h
+  go run ./cobra-example/  echo times -h
+
+  go run ./cobra-example/ echo abcd efg
+  go run ./cobra-example/ echo abcd efg -p
+
+  go run ./cobra-example/ echo times abcd efg -t 3
+  go run ./cobra-example/ echo times abcd efg -p -t 3
+
+  go run ./cobra-example/ -p -s "a string" echo something
+  go run ./cobra-example/ echo something -p -s "a string"
+  go run ./cobra-example/ echo something -ps "a string"
+
+  # required flag
+  go run ./cobra-example/  echo times abcd efg
+
+  # to see returned custom error
+  go run ./cobra-example/  echo times abcd efg --persistFlag=true -t 4
+  ```
+
+* Hello
+
+  ```bash
+  go run ./cobra-example/ hello -h
+  ```
+
+* Validargs 
+
+  ```bash
+  # valid call
+  go run ./cobra-example/ validargs three two
+
+  # throw error for invalid argument
+  go run ./cobra-example/ validargs five
+  ```
--- cobra-example/main.go
+++ cobra-example/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+
+	// "log"
 	"strings"
 
 	"github.com/spf13/cobra"
@@ -13,23 +14,29 @@ var (
 	persistRootFlag bool
 	localRootFlag bool
 	times int
+	pfRootString string
 	rootCmd = &cobra.Command{
-		Use: "cobra-app",
+		Use: "app1",
 		Short: "An example cobra program",
 		Long: `This is a simple example of a cobra program.
-It will have several subcommands and flags.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello from the root command")
-		},
+It will have several subcommands and flags.
+
+Examples:
+# to set flags:
+go run ./cobra-example/ echo something -ps "a string"
+`,
+		// Run: func(cmd *cobra.Command, args []string) {
+		// 	fmt.Printf("Hello from the root command: persistFlag=%v, localFlag=%v\n", persistRootFlag, localRootFlag)
+		// },
 	}
 	echoCommand = &cobra.Command{
 		Use: "echo [strings to echo]",
 		Short: "prints given strings to stdout",
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(args)
-			p, _ := rootCmd.Flags().GetBool("persistFlag")
-			fmt.Printf("Echo: %v - persistentFlag: %v\n", strings.Join(args, " "), p)
+			// fmt.Println(args)
+			// p, _ := rootCmd.Flags().GetBool("persistFlag")
+			fmt.Printf("Echo: %v - persistentFlag: %v, pfString: %q\n", strings.Join(args, " "), persistRootFlag, pfRootString)
 		},
 	}
 	timesCommand = &cobra.Command{
@@ -38,11 +45,11 @@ It will have several subcommands and flags.`,
 		Args: cobra.MinimumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			if times == 4 {
-				// return fmt.Errorf("don't use times = 4")
-				return errors.New("don't use times == 4")
+				return fmt.Errorf("don't use times = 4")
+				// return errors.New("don't use times == 4")
 			}
 			for i := 0; i < times; i++ {
-				fmt.Printf("Echo times: %v - persistent flag: %v\n", strings.Join(args, " "), persistRootFlag)
+				fmt.Printf("Echo times: %v - persistent flag: %v, pfString: %q\n", strings.Join(args, " "), persistRootFlag, pfRootString)
 			}
 			return nil
 		},
@@ -63,23 +70,36 @@ It will have several subcommands and flags.`,
 			fmt.Printf("Hello, World: %v\n", args)
 		},
 	}
+	validArgsCommand = &cobra.Command{
+		Use: "validargs",
+		Short: "An example of valid args",
+		Args: cobra.OnlyValidArgs,
+		ValidArgs: []string{"one", "two","three"},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("Valid args are: %v\n", args)
+		},
+	}
 )
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlag", "p", false, "a persistent root flag")
-	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", true, "a local root flag")
+	rootCmd.PersistentFlags().StringVarP(&pfRootString, "pfString", "s", "", "a persistent string flag on root")
+	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
 	rootCmd.AddCommand(echoCommand)
 
-	timesCommand.Flags().IntVarP(&times, "times", "t", 1, "number of times to echo to stdout")
+	timesCommand.Flags().IntVarP(&times, "times", "t", 0, "number of times to echo to stdout")
 	timesCommand.MarkFlagRequired("times")
 	echoCommand.AddCommand(timesCommand)
 
 	// custom argument validation
 	rootCmd.AddCommand(helloCommand)
+
+	// example of valid args list
+	rootCmd.AddCommand(validArgsCommand)
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
-		log.Fatal(err)
+		// log.Fatal(err)
 	}
 }
